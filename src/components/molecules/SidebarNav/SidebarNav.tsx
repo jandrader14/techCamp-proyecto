7d87154ea8d49fba43177fe3db8200e9edbca7bf
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-05 16:06:37 -0500

    :sparkles: Sidebar component

--- src/components/molecules/SidebarNav/SidebarNav.tsx
+++ src/components/molecules/SidebarNav/SidebarNav.tsx
@@ -1,28 +1,23 @@
-import { Home, Settings, Boxes } from "lucide-react";
+import { Home, Settings, Archive, BookHeart, Bell } from "lucide-react";
 import styles from "./SidebarNav.module.css";
 
 export function SidebarNav() {
   return (
-    <aside className="bg-white shadow-md w-64 h-screen p-4 fixed top-0 left-0">
-      <h1 className="text-2xl font-bold mb-8 text-center">Mi Inventario</h1>
-      <nav className="space-y-4">
-        <a
-          href="#"
-        >
-          <Home size={20} /> Inicio
-        </a>
-        <a
-          href="#"
-          
-          <Boxes size={20} /> Inventario
-        </a>
-        <a
-          href="#"
-          
-        >
-          <Settings size={20} /> Configuración
-        </a>
-      </nav>
-    </aside>
+    <nav className={styles.navContainer}>
+        <div className={styles.navContent}>
+            <div className={styles.logo}>
+                <img src="src/assets/vegetales1.png" />Logo dashboard</div>
+            <div className={styles.icons}>
+                <Home size={24} />Inicio</div>
+            <div className={styles.icons}>
+                <Archive size={24} />Inventario</div>
+            <div className={styles.icons}>
+                <BookHeart />Recetas</div>
+            <div className={styles.icons}>
+                <Bell />Notificaciones</div>
+            <div className={styles.icons}>
+                <Settings size={24}/>Ajustes</div>
+        </div>
+    </nav>
   );
 }
