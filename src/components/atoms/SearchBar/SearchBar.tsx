f4484ea4ea9142af255f6f3392bd6be427c0c295
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-08 16:34:07 -0500

    Build dashboard layout

--- src/components/atoms/SearchBar/SearchBar.tsx
+++ src/components/atoms/SearchBar/SearchBar.tsx
@@ -1,1 +1,5 @@
-import React from 'react';
+export function SearchBar() {
+    return (
+        <div></div>
+     )
+}
--- src/components/organisms/DashboardLayout/DashboardLayout.tsx
+++ src/components/organisms/DashboardLayout/DashboardLayout.tsx
@@ -1,13 +1,15 @@
 import { SidebarNav } from "../SidebarNav/SidebarNav";
-import { SearchBar } from "../SearchBar/SearchBar";
-import { WelcomeCard } from "../WelcomeCard/WelcomeCard";
+import { SearchBar } from "../../atoms/SearchBar/SearchBar";
+import { WelcomeCard } from "../../molecules/WelcomeCard/WelcomeCard";
 import { CategorySection } from "../CategorySection/CategorySection";
+import {Header} from "../../atoms/Header/Header";
 import styles from "./DashboardLayout.module.css";
 
 export function DashboardLayout() {
   return (
     <div className={styles.container}>
       <SidebarNav />
+      <Header />
       <main className={styles.content}>
         <SearchBar />
         <WelcomeCard />
