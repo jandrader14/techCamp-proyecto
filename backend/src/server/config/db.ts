8e1042f980e948ea006d9eb0ba72e5cfa68fa198
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-08 22:47:34 -0500

    :sparkles: Create DB configuration

--- backend/src/server/config/db.ts
+++ backend/src/server/config/db.ts
@@ -3,14 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDB = async () => {
+export async function connectDB() {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI!);
-    console.log(`MongoDB conectado: ${conn.connection.host}`);
+    //console.log(`MongoDB conectado: ${conn.connection.host}`);
+    console.log('Conexión exitosa a la base de datos ✅')
   } catch (error) {
     console.error("Error conectando a MongoDB:", error);
-    process.exit(1);
+    process.exit(1); //Stop the connection if server fail
   }
 };
 
-export default connectDB;
+
