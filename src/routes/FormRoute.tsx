2c02e94e659c20ebbb1a9d662fefac7b1734d201
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-16 21:07:24 -0500

    :wrench: Update route for dashboard rendering

--- src/routes/FormRoute.tsx
+++ src/routes/FormRoute.tsx
@@ -1,20 +1,20 @@
 //import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router";
 
-import {Login} from "../components/pages/Login/Login";
+import { Login } from "../components/pages/Login/Login";
 import { Register } from "../components/pages/Register/Register";
-import {ForgotPasswordForm} from "../components/organisms/ForgotPasswordForm/ForgotPasswordForm";
-import {Dashboard} from "../components/pages/Dashboard/Dashboard";
+import { ForgotPasswordForm } from "../components/organisms/ForgotPasswordForm/ForgotPasswordForm";
+import { Dashboard } from "../components/pages/Dashboard/Dashboard";
+import { DashboardLayout } from "../components/layout/DashboardLayout/DashboardLayout";
 
 export const FormRoute: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register/>} />
+      <Route path="/register" element={<Register />} />
       <Route path="/forgotPassword" element={<ForgotPasswordForm />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<DashboardLayout />} />
+      <Route index element={<Dashboard />} />
     </Routes>
   );
 };
-
-
