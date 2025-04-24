740bfdc05be9bcbce2bfb4c09b88dce74efe6d65
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-24 12:07:38 -0500

    :lipstick: Contianer for image

--- src/components/molecules/ProductCard/ProductCard.tsx
+++ src/components/molecules/ProductCard/ProductCard.tsx
@@ -6,7 +6,7 @@ import { Pencil, Trash2 } from "lucide-react";
 interface ProductCardProps extends Pick<Product, "image" | "name" | "quantity" | "expirationDate"> {
   //Con Pick<Product, "..."> se está usando una parte del tipo Product
   onEdit?: () => void;
-  onDelete?: () => void;
+  onDelete: () => void;
 }
 
 export function ProductCard({
@@ -14,19 +14,22 @@ export function ProductCard({
   name,
   quantity,
   expirationDate,
+  onDelete,
 }: ProductCardProps) {
   return (
     <div className={styles.card}>
-      <img src={image} alt={name} className={styles.image} />
+      <div className={styles.imageWrapper}>
+        <img src={image} alt={name} className={styles.image} />
+      </div>
       <div className={styles.info}>
         <div className={styles.description}>
           <h3>{name}</h3>
           <span>Cantidad: {quantity}</span>
-          <span>Fecha de vencimiento: {expirationDate}</span>
+          <span>Vence: {expirationDate}</span>
         </div>
         <div className={styles.buttons}>
           <Button type="button" text="Editar" className={styles.editBtn}><Pencil size={16} /></Button>
-          <Button type="button" text="Eliminar" className={styles.deleteBtn}><Trash2 size={16} /></Button>
+          <Button type="button" text="Eliminar" className={styles.deleteBtn} onClick={onDelete}><Trash2 size={16} /></Button>
         </div>
       </div>
     </div>
