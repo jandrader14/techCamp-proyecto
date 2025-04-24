720fe02bac4d18a01ac331049da0e45ad23a2560
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-24 10:08:31 -0500

    :art: Improve structure and props for the Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-//import styles from "./Button.module.css";
+import React from "react";
 
-interface ButtonProps {
-  text: string;
-  type?: "button" | "submit";
-  onClick?: () => void;
-  disabled?: boolean;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  //extends React.ButtonHTMLAttributes<HTMLButtonElement> hereda todas las propiedas estandar de un <button>
+  text?: string;
   className?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, type = "button", onClick, className }) => {
+export const Button: React.FC<ButtonProps> = ({
+  text,
+  type = "button",
+  onClick,
+  className,
+  children,
+  ...props
+}) => {
   return (
-    <button className={className} type={type} onClick={onClick} >
-      {text}
+    <button
+      type={type}
+      onClick={onClick}
+      className={className}
+      {...props}
+    >
+      {children ? children : text}
     </button>
   );
 };
-
