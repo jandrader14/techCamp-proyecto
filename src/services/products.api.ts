b421777a62e080713c867ac10db98a6f69a9bfd5
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-24 11:51:34 -0500

    :sparkles: deleteProducts function

--- src/services/products.api.ts
+++ src/services/products.api.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
+const ProdEndpoint = "http://localhost:5000/api/products";
+
 export const productApi = {
   getAll: async () => {
-    const response = await axios.get('http://localhost:5000/api/products');
+    const response = await axios.get(ProdEndpoint);
     return response.data;
   },
+
+  deleteProduct: async (productId: string) => {
+    const response = await axios.delete(`${ProdEndpoint}/${productId}`);
+    return response.data;
+  }
 };
