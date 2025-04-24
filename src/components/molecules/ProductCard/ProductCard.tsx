7c1f48535e2ab998db4ce4655516341309c48e8a
Johanna Andrade <andrader.johanna@gmail.com>
2025-04-24 10:04:14 -0500

    :lipstick: Product Card styles and :construction: structure

--- src/components/molecules/ProductCard/ProductCard.tsx
+++ src/components/molecules/ProductCard/ProductCard.tsx
@@ -1,6 +1,7 @@
-import styles from "./ProductCard.module.css";
 import { Button } from "../../atoms/Button/Button";
 import { Product } from "../../../types/product";
+import styles from "./ProductCard.module.css";
+import { Pencil, Trash2 } from "lucide-react";
 
 interface ProductCardProps extends Pick<Product, "image" | "name" | "quantity" | "expirationDate"> {
   //Con Pick<Product, "..."> se está usando una parte del tipo Product
@@ -18,14 +19,15 @@ export function ProductCard({
     <div className={styles.card}>
       <img src={image} alt={name} className={styles.image} />
       <div className={styles.info}>
-        <h3>{name}</h3>
-        <span>{quantity}</span>
-        <span>{expirationDate}</span>
-      </div>
-
-      <div className={styles.buttons}>
-        <Button type="submit" text="" className={styles.editBtn} />
-        <Button type="submit" text="" className={styles.deleteBtn} />
+        <div className={styles.description}>
+          <h3>{name}</h3>
+          <span>Cantidad: {quantity}</span>
+          <span>Fecha de vencimiento: {expirationDate}</span>
+        </div>
+        <div className={styles.buttons}>
+          <Button type="button" text="Editar" className={styles.editBtn}><Pencil size={16} /></Button>
+          <Button type="button" text="Eliminar" className={styles.deleteBtn}><Trash2 size={16} /></Button>
+        </div>
       </div>
     </div>
   );
