69a9b7206ef542303e8c59a9b85d278cb12a7874
Johanna Andrade <andrader.johanna@gmail.com>
2025-05-02 15:24:37 -0500

    :sparkles: Add New Route for Recipe

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import UserRoute from  './user.route.ts'
 import ProductRoute from './products.routes.ts';
+import RecipeRoute from './recipe.route.ts';
 
 
 export const routes = {
     UserRoute, 
-    ProductRoute
+    ProductRoute,
+    RecipeRoute
 }
