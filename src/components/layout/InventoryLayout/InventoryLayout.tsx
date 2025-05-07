6083528bf857625e79c06b3d5fd6539c32b14f27
Johanna Andrade <andrader.johanna@gmail.com>
2025-05-06 22:29:35 -0500

    :recycle: Refactor code, in order to centralize Add Products and Get products Logic

--- src/components/layout/InventoryLayout/InventoryLayout.tsx
+++ src/components/layout/InventoryLayout/InventoryLayout.tsx
@@ -1,10 +1,41 @@
+import  { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { SidebarNav } from "../../organisms/SidebarNav/SidebarNav";
 import { Header } from "../../organisms/Header/Header";
-
+import { productApi } from "../../../services/products.api"; // Importa tu servicio de API
+import { Product } from "../../../types/product"; // Importa tu tipo de Product
 import styles from "./InventoryLayout.module.css";
 
 export function InventoryLayout() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = async () => {
+    try {
+      const data = await productApi.getAll();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error al obtener productos:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleAddNewProduct = async (
+    newProduct: Omit<Product, "_id" | "__v">
+  ) => {
+    try {
+      await productApi.createProduct(newProduct);
+      await fetchProducts(); // Recarga la lista después de agregar
+    } catch (error) {
+      console.error("Error al guardar el nuevo producto:", error);
+    }
+  };
+
   return (
     <div className={styles.inventoryContainer}>
       <SidebarNav />
@@ -12,7 +43,17 @@ export function InventoryLayout() {
         <Header />
         <main className={styles.mainSection}>
           <div className={styles.mainContent}>
-            <Outlet /> {/* Aquí se mostrarán las subpáginas del inventario */}
+            {loading ? (
+              <p>Cargando productos...</p>
+            ) : (
+              <Outlet
+                context={{
+                  products: products,
+                  fetchProducts: fetchProducts,
+                  onAddProduct: handleAddNewProduct,
+                }}
+              />
+            )}
           </div>
         </main>
       </div>
