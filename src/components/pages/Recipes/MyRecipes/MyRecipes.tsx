5514c30f53533de2025bc1ce21d6eb86612abdd5
Johanna Andrade <andrader.johanna@gmail.com>
2025-05-13 09:08:06 -0500

    :sparkles: Folders for recipes sections

---
+++ src/components/pages/Recipes/MyRecipes/MyRecipes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const MyRecipes: React.FC = () => {
+  return (
+    <div>
+      <h1>Mis Recetas</h1>
+      <p>Aquí puedes ver tus recetas guardadas.</p>
+    </div>
+  );
+}
---
+++ src/components/pages/Recipes/RecipesIA/RecipesIA.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const RecipesIA: React.FC = () => {
+    return (
+        <div>
+        <h1>Recetas IA</h1>
+        <p>Aquí puedes ver recetas recomendadas por IA.</p>
+        </div>
+    );
+    }
