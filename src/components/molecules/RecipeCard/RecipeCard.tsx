5369b400ab633c58c09ab16d59a6df6ef303c2cc
Johanna Andrade <andrader.johanna@gmail.com>
2025-05-14 21:46:29 -0500

    :lipstick: Modify and Improve styles for Recipe Card

--- src/components/molecules/RecipeCard/RecipeCard.tsx
+++ src/components/molecules/RecipeCard/RecipeCard.tsx
@@ -1,9 +1,9 @@
-import styles from "./RecipeCard.module.css";
-//import axios from "axios";
 import { Button } from "../../atoms/Button/Button";
 import { Search } from "lucide-react";
 
-interface RecipeCardProps {
+import styles from "./RecipeCard.module.css";
+
+export interface RecipeCardProps {
   id?: string;
   title: string;
   description: string;
@@ -13,13 +13,15 @@ interface RecipeCardProps {
   maxTitleLength?: number;
 }
 
-export const RecipeCard: React.FC<RecipeCardProps> = ({id,  title, description, image, onViewDetails, className, maxTitleLength }) => {
+export const RecipeCard: React.FC<RecipeCardProps> = ({  title, description, image, onViewDetails, className, maxTitleLength }) => {
   const shortTitle = maxTitleLength && title.length > maxTitleLength
     ? title.substring(0, maxTitleLength) + "..."
     : title;
   return (
     <div className={`${styles.card} ${className || ""}`}>
-      <img src={image} alt={title} className={styles.image} />
+      <div className={styles.imageContainer}>
+        <img src={image} alt={title} className={styles.image} />
+      </div>
 
       <div className={styles.content}>
         <h3 className={styles.title}>{shortTitle}</h3>
